e13d422a4e5c6c280d43cb045684df1fe27f84ed
PRITAM RAJ <pritamrajbps@gmail.com>
2023-05-28 20:11:28 +0530

    simple code for taking user input and how print statement works

---
+++ Day1/main.py
@@ -0,0 +1,11 @@
+print("Hello world!\nHello world!\n")
+print("Hello" + "Pritam")
+print("Hello" + " " +"Pritam")
+
+##print("What is your name")
+
+##To take User input fromm the user
+##input("What is your name?")
+
+##Nesting of "User input" inside the print
+print("Hello " +input("What is your name?"))
