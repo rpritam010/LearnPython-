aed553a4122dfe466e59a52bdbf885b2d78dddbc
PRITAM RAJ <pritamrajbps@gmail.com>
2023-05-28 20:30:32 +0530

    variable and length function

---
+++ Day1/function.py
@@ -0,0 +1,6 @@
+##To find the length of the input
+
+print(len(input("What is your name?")))
+#The above code will give the below output
+#What is your name?Pritam
+#6
---
+++ Day1/variables.py
@@ -0,0 +1,17 @@
+name = input("What is your name? ")
+print(name)
+#The above code will give the below output
+#What is your name? Pritam
+#Pritam 
+
+#Switching the values
+
+a = input("a :")
+b = input("b :")
+
+c = a;
+a = b;
+b = c;
+
+print("a is " +a)
+print("b is " +b)
