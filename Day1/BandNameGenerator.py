d77da9d5e698ba3325edfafe7046130f936b7dbf
PRITAM RAJ <pritamrajbps@gmail.com>
2023-05-28 20:47:55 +0530

    BandName generator Project

---
+++ Day1/BandNameGenerator.py
@@ -0,0 +1,24 @@
+#Create a greeting for your Program
+print("Welcome to Band Name Generator.")
+name = input("Enter your good Name! ")
+print("Hello\n" + name)
+#Ask the user for the city that they grew up in
+city = input("Which city did you grow up in \n" + name)
+print(city)
+#Ask the user for the name of a pet
+pet = input("What is the name of your pet")
+#Combine the name of their city and pet and show them their band name.
+print("Your Band Name Could Be \n" +city + " "+ pet)
+#Make sure the input cursor shows on a new line
+
+
+''' The above code will give the following output :- Welcome to Band Name Generator.
+Enter your good Name! Pritam
+Hello
+Pritam
+Which city did you grow up in
+Pritam Purnia
+ Purnia
+What is the name of your pet Oggy
+Your Band Name Could Be 
+ Purnia  Oggy'''
