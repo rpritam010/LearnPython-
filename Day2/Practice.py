a1f72725ba8f5f0bf834757e7d8426a15f3219c1
PRITAM RAJ <pritamrajbps@gmail.com>
2023-06-04 13:28:46 +0530

    challange & the datatype

---
+++ Day2/Practice.py
@@ -0,0 +1,11 @@
+num_char = len(input("What is your name? "))
+
+#Check the datatype
+print(type(num_char))
+
+#Type cast
+new_num_char = str(num_char)
+print("Your name has "+ new_num_char + " characters.")
+
+a = str(123)
+print(type(a))
---
+++ Day2/challange_1.py
@@ -0,0 +1,12 @@
+''' Write a program that adds the digits in a 2 digit number. eg if the input is 35then the output should be 3 +5 =8'''
+
+two_digit_number = input("Enter the two digit number ")
+
+print(type(two_digit_number))
+
+#typecast and add the didgit to product output
+print(int(two_digit_number[0]) + int(two_digit_number[1]))
+
+#PEMDASLR --- PERNANTHESIS -- {}/(),EXPONENTS-- **,MULTIPLICATION-- * & DIVISION-- /, ADDITION-- + ,SUBSTRACTION-- - , LEFT,RIGHT
+
+print(3*3+3/3-3)## will give the output as 7.0
