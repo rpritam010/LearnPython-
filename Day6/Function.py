b0f12954e3224fbb1ac7c6c7f3cb0b9fd0448fbc
PRITAM RAJ <pritamrajbps@gmail.com>
2023-06-29 21:47:36 +0530

    day6 Started

---
+++ Day6/Function.py
@@ -0,0 +1,11 @@
+print("Hello")
+num_char =len("Hello")
+print(num_char)
+
+''' How to create a function'''
+def my_function():
+    print("Hello")
+    print("Bye")
+
+'''Calling function "my_function'''
+my_function()
